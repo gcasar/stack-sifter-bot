f71b6284c53e36b4542ce6b2d4809bb477dc2142
gcasar <gregorcasar@gmail.com>
2025-07-05 19:05:09 +0000

    only output json

--- src/StackSifter/Program.cs
+++ src/StackSifter/Program.cs
@@ -8,7 +8,6 @@ if (args.Length == 0 || !DateTime.TryParse(args[0], null, System.Globalization.D
     Console.WriteLine("Please provide a UTC timestamp as the first argument (e.g., 2025-07-05T12:34:56Z)");
     return;
 }
-Console.WriteLine($"Last run timestamp (UTC): {since:O}");
 
 // Wire things up!
 IPostsFeed feed = new StackOverflowRSSFeed();

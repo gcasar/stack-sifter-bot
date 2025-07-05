e0a37ec3837df29d05d9ced4425c8b8824da5886
gcasar <gregorcasar@gmail.com>
2025-07-05 16:24:13 +0000

    simple fetch to show rss reader working

--- src/StackSifter/Program.cs
+++ src/StackSifter/Program.cs
@@ -1,7 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-if (args.Length > 0)
+﻿using StackSifter.Feed;
+// Require UTC timestamp as argument
+if (args.Length == 0 || !DateTime.TryParse(args[0], null, System.Globalization.DateTimeStyles.AdjustToUniversal | System.Globalization.DateTimeStyles.AssumeUniversal, out var since))
 {
-    Console.WriteLine($"Last run timestamp: {args[0]}");
+    Console.WriteLine("Please provide a UTC timestamp as the first argument (e.g., 2025-07-05T12:34:56Z)");
+    return;
+}
+Console.WriteLine($"Last run timestamp (UTC): {since:O}");
+
+// Configure IPostsFeed to use StackOverflowRSSFeed
+IPostsFeed feed = new StackOverflowRSSFeed();
+var posts = await feed.FetchPostsSinceAsync(since);
+
+foreach (var post in posts)
+{
+    Console.WriteLine($"[{post.Published:O}] {post.Title}\nTags: {string.Join(", ", post.Tags)}\n");
 }

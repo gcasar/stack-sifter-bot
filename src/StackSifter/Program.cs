87bebcf3ef7260d401e2958399000fd535ff464f
Gregor Casar <gregorcasar@gmail.com>
2025-07-05 14:12:38 +0000

    track last run information
    this will later ensure we're processing elements as they come in a very naive way.
    
    it also shows we're able to store some data

--- src/StackSifter/Program.cs
+++ src/StackSifter/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+if (args.Length > 0)
+{
+    Console.WriteLine($"Last run timestamp: {args[0]}");
+}
